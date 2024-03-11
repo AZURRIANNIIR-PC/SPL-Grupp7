4c75550bb880445a5d016fa7d659a05027b53499
NoWen22 <nora@norrkross.com>
2024-03-11 15:06:14 +0100

    NPC and questgiver
    Interaction for questgiver and NPC-child. New script, gameobjects and a check to see if quest is complete before changing the text.

---
+++ Assets/Scripts/NPC_textbubble.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC_textbubble : MonoBehaviour {
+    [SerializeField] private GameObject textparent;
+    [SerializeField] private Text text;
+    [SerializeField] private string conversationStarter;
+    //[SerializeField] private string conversationEnder;
+    //[SerializeField] private AudioSource audioS;
+    //[SerializeField] private AudioClip clip;
+
+    void Start() {
+        textparent.SetActive(false); // Gör texten osynlig
+        //textComponent.text = conversationStarter; //Sätt texten till urspungliga
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player") == true) { //Om spelaren integrerar
+            textparent.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Player") == true) {
+            textparent.SetActive(false);
+        }
+    }
+}
