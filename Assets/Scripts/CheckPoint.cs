8dcee26cabc819144b75a911b3520c3e51f121fa
linnlie <113351660+linnlie@users.noreply.github.com>
2024-03-12 14:33:23 +0100

    ändrat respawn sprite

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    [SerializeField] private Sprite checkPointUnlocked;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") == true)
         {
             collision.GetComponent<PlayerState>().ChangeRespawnPosition(gameObject);
+            gameObject.GetComponent<SpriteRenderer>().sprite = checkPointUnlocked;
         }
     }
 }
